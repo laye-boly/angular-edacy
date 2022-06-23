fc94f9be8dde6df152f9756d6ce877d7ea6f03f3
laye-boly <layeboly58@gmail.com>
2022-06-23 21:25:02 +0000

    material : menu

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
-const MODULES = [MatSliderModule, MatButtonModule];
+const MODULES = [MatSliderModule, MatButtonModule, MatMenuModule];
 
 @NgModule({
   imports: MODULES,
